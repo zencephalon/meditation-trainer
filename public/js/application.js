5e0a3bbff06358a9c2701a9fe85b7ef5f64caf77
Matthew Bunday <zenceph@gmail.com>
2014-11-29 12:25:36 -0500

    Create a meditation model on first space press.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  var meditation;
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  Mousetrap.bind('space', function() {
+    if (!meditation) {
+      mediation = new Meditation(MAX_BREATHS, $('#prompt'), $('#timer_display'));
+    }
+    console.log("Hello!");
+  })
 });
