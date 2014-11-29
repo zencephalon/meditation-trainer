5886083e0e4b0d29b81219bdd3827c0a6163090a
Matthew Bunday <zenceph@gmail.com>
2014-11-29 14:42:15 -0500

    Add error handling helpers.

---
+++ app/helpers/error_handling.rb
@@ -0,0 +1,14 @@
+def display_error
+  error = session[:error]
+  session[:error] = nil
+
+  if error
+    return erb :'errors/error_display', layout: false, locals: {errors: error}
+  else
+    return ""
+  end
+end
+
+def set_error(msg)
+  session[:error] = {"Error" => [msg]}
+end
