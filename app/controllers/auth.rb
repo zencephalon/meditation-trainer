f2c7d391e9bc855679e3d1cc71e5eb040836e409
Matthew Bunday <zenceph@gmail.com>
2014-11-29 14:39:53 -0500

    Working on auth controller.

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,7 +1,35 @@
 get '/login' do
-  
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name]).try(:authenticate, params[:user][:password])
+
+  if user
+    session[:user_id] = user.id
+    redirect("/")
+  else
+    set_error("Username not found or password incorrect.")
+    redirect("/login")
+  end
 end
 
 get '/signup' do
-  
+  erb :'auth/signup'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect("/")
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect("/")
+  else
+    session[:error] = user.errors.messages 
+    redirect("/signup")
+  end
 end
