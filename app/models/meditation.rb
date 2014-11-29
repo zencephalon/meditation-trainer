529e4a19d8eae0e68862267f1bdaac9dc2d81ab2
Matthew Bunday <zenceph@gmail.com>
2014-11-29 15:24:14 -0500

    Table row display and format time helper for meditation model.

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -5,9 +5,23 @@ class Meditation < ActiveRecord::Base
     html = ""
     %w(inhale exhale cycle).each do |phase|
       %w(min max avg).each do |stat|
-        html += "#{phase} #{stat}: <span class='time'>#{self.send(phase + "_" + stat).round(1)}</span> s<br>"
+        html += "#{phase} #{stat}: #{format_time(self.send(phase + "_" + stat))} s<br>"
       end
     end
     html
+  end
+
+  def format_time(time)
+    "<span class='time'>#{time.round(1)}</span>"
+  end
+
+  def table_row
+    html = "<tr><td><a href='/meditation/#{self.id}'>#{self.id}</a></td>"
+    %w(inhale exhale cycle).each do |phase|
+      %w(min max avg).each do |stat|
+        html += "<td>#{format_time(self.send(phase + "_" + stat))}</td>"
+      end
+    end
+    html + "</tr>"
   end
 end
