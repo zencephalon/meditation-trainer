f38abb6017be5d666d314006bd9405446438255f
Matthew Bunday <zenceph@gmail.com>
2014-11-29 12:25:54 -0500

    initTimer and setPrompt on meditation model

--- public/js/meditation.js
+++ public/js/meditation.js
@@ -1,5 +1,25 @@
-function Meditation(max_breaths) {
+function Meditation(max_breaths, $prompt, $timer_display) {
   this.max_breaths = max_breaths;
   this.breaths = {inhale: [], exhale: []};
-  this.cycle = "inhale";
+  this.phase = "inhale";
+  this.phase_start = Date.now();
+  this.$prompt = $prompt;
+  this.$timer_display = $timer_display;
+  this.initTimer();
+  this.setPrompt();
+}
+
+Meditation.prototype.setPrompt = function() {
+  this.$prompt.html(this.phase);
+}
+
+Meditation.prototype.initTimer = function() {
+  var self = this;
+  this.timerInterval = setInterval(function() {
+    self.$timer_display.html(self.timeDiff());
+  }, 100);
+}
+
+Meditation.prototype.timeDiff = function() {
+  return ((Date.now() - this.phase_start) / 1000).toFixed(1)
 }
