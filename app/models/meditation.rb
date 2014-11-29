7f36765d0a3924a55bbcea85751cca7f3082b5c3
Matthew Bunday <zenceph@gmail.com>
2014-11-29 15:07:46 -0500

    Write stats_summary method for meditation model.

--- app/models/meditation.rb
+++ app/models/meditation.rb
@@ -1,3 +1,13 @@
 class Meditation < ActiveRecord::Base
   belongs_to :user
+
+  def stats_summary
+    html = ""
+    %w(inhale exhale cycle).each do |phase|
+      %w(min max avg).each do |stat|
+        html += "#{phase} #{stat}: <span class='time'>#{self.send(phase + "_" + stat).round(1)}</span> s<br>"
+      end
+    end
+    html
+  end
 end
