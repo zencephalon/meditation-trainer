2a5943d9899a875d5d736451d71f9ae8389f1591
Matthew Bunday <zenceph@gmail.com>
2014-11-29 15:35:35 -0500

    Give user a stats summary method too

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :meditations
   # Remember to create a migration!
+
+  def stats_summary
+    html = ""
+    %w(inhale exhale cycle).each do |phase|
+      %w(min max avg).each do |stat|
+        time = format_time(self.meditations.map {|med| med.send(phase + "_" + stat)}.max)
+        html += "record #{phase} #{stat}: #{time}<br>"
+      end
+    end
+    html
+  end
+
+  def format_time(time)
+    "<span class='time'>#{time.round(1)}</span> s"
+  end
 end
